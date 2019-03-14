2f520cbd351fcb82272326f6816d650c856280d2
RoyIronGrey <15665512229@163.com>
2019-03-14 08:44:05 +0800

    -news_list in index start  2019/3/14-

--- src/main/java/cn/edu/csu/information/controller/IndexController.java
+++ src/main/java/cn/edu/csu/information/controller/IndexController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -28,15 +29,16 @@ public class IndexController {
     @Resource
     private IndexNewsListService indexNewsListService;
 
-//    @GetMapping("/news_list")
-//    public String news_list(Model model){
-//        Pageable newsOrderedPageable = PageRequest.of(0,6);
-//        Page<InfoNews> newsOrderedPage = indexNewsListService.findAll(newsOrderedPageable);
-//
-//        System.out.println("总条数："+newsOrderedPage.getTotalElements());
-//        System.out.println("总页数："+newsOrderedPage.getTotalPages());
-//        return null;
-//    }
+    @GetMapping("/news_list")
+    @ResponseBody
+    public String news_list(){
+        Pageable newsOrderedPageable = PageRequest.of(0,6);
+        Page<InfoNews> newsOrderedPage = indexNewsListService.findAll(newsOrderedPageable);
+
+        System.out.println("总条数："+newsOrderedPage.getTotalElements());
+        System.out.println("总页数："+newsOrderedPage.getTotalPages());
+        return "I am news_list";
+    }
 
     @RequestMapping("/")
     public String index(Model model){
--- src/main/java/cn/edu/csu/information/controller/NewsListController.java
+++ src/main/java/cn/edu/csu/information/controller/NewsListController.java
@@ -7,23 +7,25 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
 public class NewsListController {
-    @Resource
-    private IndexNewsListService indexNewsListService;
+//    @Resource
+//    private IndexNewsListService indexNewsListService;
 
-    @GetMapping("/news_list")
-    public String news_list(Model model){
-        Pageable newsOrderedPageable = PageRequest.of(0,6);
-        Page<InfoNews> newsOrderedPage = indexNewsListService.findAll(newsOrderedPageable);
-
-        System.out.println("总条数："+newsOrderedPage.getTotalElements());
-        System.out.println("总页数："+newsOrderedPage.getTotalPages());
-        return "I am news_list";
+    @GetMapping("/test")
+    public String news_list(){
+//        Pageable newsOrderedPageable = PageRequest.of(0,6);
+//        Page<InfoNews> newsOrderedPage = indexNewsListService.findAll(newsOrderedPageable);
+//
+//        System.out.println("总条数："+newsOrderedPage.getTotalElements());
+//        System.out.println("总页数："+newsOrderedPage.getTotalPages());
+//        return "I am news_list";
+        return "I am test";
     }
 
 
